13ca82bc20ef2fe9cb570de0c1c4003511fb3c17
PePetrov96 <sstrigger96@gmail.com>
2024-06-02 14:38:21 +0300

    update

--- src/main/java/com/project/EpicByte/model/entity/BaseEntity.java
+++ src/main/java/com/project/EpicByte/model/entity/BaseEntity.java
@@ -4,14 +4,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @MappedSuperclass
-@Getter @Setter @NoArgsConstructor
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public BaseEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/project/EpicByte/model/entity/User.java
+++ src/main/java/com/project/EpicByte/model/entity/User.java
@@ -3,14 +3,21 @@ package com.project.EpicByte.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "user")
-@Getter @Setter @NoArgsConstructor
 public class User extends BaseEntity {
     @Column(name = "username")
     private String username;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
